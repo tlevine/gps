58c93877a6bea4c60289464190ecc0b92ea7f3b5
Thomas Levine <_@thomaslevine.com>
2015-04-11 07:00:36 +0000

    download tracks script

--- landelijke-fietsroutes/tracks.py
+++ landelijke-fietsroutes/tracks.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 # http://en.nederlandfietsland.nl/en/long-distance-cycle-routes/gps-tracks
+import zipfile, io, sys
+
 import lxml.html, requests
 
-r = requests.get('http://en.nederlandfietsland.nl/en/long-distance-cycle-routes/gps-tracks')
+url = 'http://en.nederlandfietsland.nl/en/long-distance-cycle-routes/gps-tracks'
+r = requests.get(url)
 html = lxml.html.fromstring(r.text)
-print(html.xpath('//a[contains(".zip", @src)]/@src))
+html.make_links_absolute(url)
+for zipped_track in html.xpath('//a[contains(@href, ".zip")]/@href'):
+    r = requests.get(zipped_track)
+    try:
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+    except zipfile.BadZipFile:
+        sys.stdout.write('%s is not a zip file, skipping\n' % zipped_track)
+    else:
+        z.extractall()

08cfb9f1d09352d7e4e3c289bc3accee03b2d580
Thomas Levine <_@thomaslevine.com>
2015-04-12 05:49:58 +0000

    parallel

--- eurovelo/unsegment.py
+++ eurovelo/unsegment.py
@@ -1,25 +1,27 @@
 import os
+from concurrent.futures import ThreadPoolExecutor
 
 import lxml.etree
 
 def main():
-    for gpx_fn in os.listdir():
-        if not gpx_fn.endswith('.gpx'):
+    with ThreadPoolExecutor(4) as e:
+        for fn in os.listdir():
+            if fn.endswith('.gpx'):
+                e.submit(convert_file, fn)
+
+def convert_file(old_fn):
+    gpx = lxml.etree.iterparse(old_fn)
+    basename = gpx.partition('.')[0]
+    i = 1
+
+    for end, element in gpx:
+        if not element.tag == '{http://www.topografix.com/GPX/1/1}trk':
             continue
+        i += 1
+        with open('%s.%d.gpx', 'wb') as fp:
+            fp.write(lxml.etree.tostring(convert_trk(element)))
 
-        gpx = lxml.etree.iterparse('EV15.gpx')
-        basename = gpx.partition('.')[0]
-        i = 1
-
-        for end, element in gpx:
-            if not element.tag == '{http://www.topografix.com/GPX/1/1}trk':
-                continue
-            fn = 
-            i += 1
-            with open('%s.%d.gpx', 'wb') as fp:
-                fp.write(lxml.etree.tostring(convert(element)))
-
-def convert(trk):
+def convert_trk(trk):
     gpx = lxml.etree.fromstring(b'''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
     <gpx version="1.1" creator="GPS Utility 4.20 - http://www.gpsu.co.uk"
       xmlns="http://www.topografix.com/GPX/1/1"
